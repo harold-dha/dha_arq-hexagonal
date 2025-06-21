d76ed17eab4f1c792d249be7e085a2391460dde8
harold-dha <harold.yontop@gmail.com>
2025-06-20 19:57:45 -0500

    feat: aplicacion http (servidor web)

--- src/prestamos/infrastructure/bootstrap/App.ts
+++ src/prestamos/infrastructure/bootstrap/App.ts
@@ -1,17 +1,11 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./AppModule";
-import { UserController } from "../controller/UserController";
 
 async function bootstrap() {
-  const appContext = await NestFactory.createApplicationContext(AppModule);
-  const userController = appContext.get(UserController);
-
-  userController.addUser("Alice");
-  userController.addUser("Bob");
-  userController.getUsers();
-
-  await appContext.close();
+  const appContext = await NestFactory.create(AppModule);
+  await appContext.listen(3000);
+  console.log("App is running on http://localhost:3000");
 }
 
 bootstrap();
--- src/prestamos/infrastructure/controller/UserController.ts
+++ src/prestamos/infrastructure/controller/UserController.ts
@@ -1,17 +1,21 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Body, Controller, Get, Logger, Post } from "@nestjs/common";
 import { UserService } from "../../application/service/UserService";
 
-@Injectable()
+@Controller("users")
 export class UserController {
   private readonly logger = new Logger(UserController.name);
   constructor(private userService: UserService) {}
 
-  public addUser(name: string): void {
+  @Post()
+  addUser(@Body("name") name: string): void {
     this.userService.createUser(name);
+    this.logger.log(`User added via HTTP: ${name}`);
   }
 
-  public getUsers(): void {
+  @Get()
+  getUsers(): { id: number; name: string }[] {
     const users = this.userService.listUsers();
-    this.logger.log(`Users: ${JSON.stringify(users)}`);
+    this.logger.log(`Users listed via HTTP: ${JSON.stringify(users)}`);
+    return users;
   }
 }
